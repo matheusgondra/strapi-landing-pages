ba947d282baa490aec72f24bc8faee1c56bdbb59
Matheus de Gondra <matheusgondra951@gmail.com>
2023-02-21 11:28:54 -0300

    Configurando jest

---
+++ src/template/App/Home.test.jsx
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+import Home from ".";
+import { renderTheme } from "../../styles/render-theme";
+import { theme } from "../../styles/theme";
+
+test("renders learn react link", () => {
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole("heading", {
+    name: "Hello"
+  }).parentElement;
+
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.secondaryBg
+  });
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule("background", "blue");
+});

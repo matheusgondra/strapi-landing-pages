1ad0f11861ba231b209d68a04ecc136ff12e772d
Matheus de Gondra <matheusgondra951@gmail.com>
2023-02-21 22:47:58 -0300

    Criando componente Heading

--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -4,12 +4,10 @@ export default {
   title: "Heading",
   component: Heading,
   args: {
-    children: "O texto está escuro",
-    light: false
+    children: "O texto está escuro"
   },
   argsTypes: {
-    children: { type: "string" },
-    light: { type: "boolean" }
+    children: { type: "string" }
   },
   parameters: {
     backgrounds: {
@@ -19,7 +17,7 @@ export default {
 };
 
 export const Light = (args) => <Heading {...args} />;
-export const Dark = (args) => <Heading {...args} light={true} />;
+export const Dark = (args) => <Heading {...args} />;
 
 Light.parameters = {
   backgrounds: {
@@ -29,5 +27,5 @@ Light.parameters = {
 
 Dark.args = {
   children: "O texto está claro",
-  light: false
+  colorDark: false
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { GlobalStyles } from "./styles/global-styles";
 import Home from "./template/App";
 import { theme } from "./styles/theme";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>

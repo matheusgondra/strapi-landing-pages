87fb092805e53b32cf40bb0c59fdb26a902314a0
Matheus de Gondra <matheusgondra951@gmail.com>
2023-02-21 22:09:37 -0300

    Adicionando fontes

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,15 +1,24 @@
 import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
+  html {
+    font-size: 62.5%;
+  }
+
   body {
-    ${({ theme }) => css`
-      /* background: ${theme.colors.mainBg}; */
-    `}
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.fonts.default};
+  }
+
+  h1 {
+    font-family: ${({ theme }) => theme.fonts.secondary};
   }
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,5 +3,9 @@ export const theme = {
     primaryColor: "#0A1128",
     secondaryColor: "#DC143C",
     white: "#FFFFFF"
+  },
+  fonts: {
+    default: "'Open Sans', sans-serif",
+    secondary: "'Montserrat', sans-serif"
   }
 };

1258043995288b1b13ccd8bbf48ce5189e377f96
Matheus de Gondra <matheusgondra951@gmail.com>
2023-03-31 13:44:54 -0300

    Criando Botaão menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,17 +3,35 @@ import P from "prop-types";
 import { SectionContainer } from "../SectionContainer";
 import { LogoLink } from "../LogoLink";
 import { NavLinks } from "../NavLinks";
+import { Menu as MenuIcon } from "@styled-icons/material-outlined/Menu";
+import { Close as CloseIcon } from "@styled-icons/material-outlined/Close";
+import { useState } from "react";
 
 export function Menu({ links = [], logoData }) {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Styled.Container>
-      <SectionContainer>
-        <Styled.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLinks links={links} />
-        </Styled.MenuContainer>
-      </SectionContainer>
-    </Styled.Container>
+    <>
+      <Styled.Button
+        onClick={() => setVisible(true)}
+        visible={visible}
+        aria-label="Open/Close menu"
+      >
+        {visible ? (
+          <CloseIcon aria-label="Close menu" />
+        ) : (
+          <MenuIcon aria-label="Open menu" />
+        )}
+      </Styled.Button>
+      <Styled.Container onClick={() => setVisible(false)} visible={visible}>
+        <SectionContainer>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </Styled.MenuContainer>
+        </SectionContainer>
+      </Styled.Container>
+    </>
   );
 }
 

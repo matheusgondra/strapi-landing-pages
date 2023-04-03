a70f056d3169672232f50dd67331229d6a9ec1b5
Matheus de Gondra <matheusgondra951@gmail.com>
2023-04-03 12:49:42 -0300

    Testando Menu

--- src/components/NavLinks/index.jsx
+++ src/components/NavLinks/index.jsx
@@ -4,7 +4,7 @@ import { MenuLink } from "../MenuLink";
 
 export function NavLinks({ links = [] }) {
   return (
-    <Styled.Container>
+    <Styled.Container aria-label="Main menu">
       {links.map((link) => (
         <MenuLink key={link} {...link} />
       ))}
